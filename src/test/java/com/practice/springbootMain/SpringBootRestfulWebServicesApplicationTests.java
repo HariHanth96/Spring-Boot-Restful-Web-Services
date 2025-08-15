b386b2208658eebc824b9051fe838c40092e8a1c
HariHanth <harihanthmanohar@gmail.com>
2025-08-15 20:15:58 +0530

    Initial Commit

---
+++ src/test/java/com/practice/springbootMain/SpringBootRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springbootMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
