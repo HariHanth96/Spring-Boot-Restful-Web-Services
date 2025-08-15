b1dfc3f07d9544cc0c7f394352ac6f43c8c09b5f
HariHanth <harihanthmanohar@gmail.com>
2025-08-15 21:28:49 +0530

    Fourth Commit

--- src/main/java/com/practice/Controller/StudentController.java
+++ src/main/java/com/practice/Controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.practice.Controller;
 
+import java.util.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.practice.Model.Student;
@@ -10,5 +12,15 @@ public class StudentController {
 	@GetMapping("/student")
 	public Student getStudent() {
 		return new Student("Hari","Hanth");
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		List<Student> students=new ArrayList<Student>();
+		students.add(new Student("Hari","Hanth"));
+		students.add(new Student("Ramesh","Padu"));
+		students.add(new Student("Vengai","Mavan"));
+		students.add(new Student("Dinesh","Likna"));
+		return students;
 	}
 }
